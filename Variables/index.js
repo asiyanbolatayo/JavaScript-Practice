68da4d489d7d4eb4dd1e742316d48526a99c08c1
Muhammed Asiyanbola <asiyanbolamuhammed.mb@gmail.com>
2025-01-16 18:52:46 +0100

    Week 1 JavaScript

---
+++ Variables/index.js
@@ -0,0 +1,44 @@
+
+let firstName = "Muhammed"
+let lastName = "Asiyanbola"
+
+let size = 20000
+let numberOfRequest = 200
+let numberOfUsers = 100
+
+const size_2 = 20
+const age = 40
+
+console.log(numberOfRequest + numberOfUsers)
+console.log(firstName, lastName, size)
+// Naming conventions
+//let message = "This is my first message" // ✅
+//let message2 = "This is my second message" // ✅
+//let user_name = "AdeJ123" // ✅
+//let myReallyLongName = "Ade Johnson" // ✅
+
+// let 1message = "This is my first message" // ❌
+//let user-name = "AdeJ123" // ❌
+// let myreallylongname = "Ade Johnson" // ❌
+
+
+// Variable declaration (const)
+// const NAME = "Ade Johnson"
+//NAME = "Adewale Johnson" //❌ //error, cannot re-assign to a constant variable
+
+//const NAME = "Ade Johnson" // ✅
+//const name = "Ade Johnson" // ❌
+
+//const MY_BIRTHDAY = "5th July 2000" // ✅
+//const MYBIRTHDAY = "5th July 2000" // ❌
+
+// Variable declaration (let)
+let fullname = "Ade Johnson"
+
+alert("Old name " + fullname)
+
+fullname = "Adewale Johnson" //✅
+alert("New name " + fullname)
+console.log(fullname)
+//const myBirthday = "5th July 2000" // ✅
+//const mybirthday = "5th July 2000" // ❌
