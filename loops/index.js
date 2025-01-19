66642e88516f56d20fba9b52706ac74c56605026
Muhammed Asiyanbola <asiyanbolamuhammed.mb@gmail.com>
2025-01-19 14:22:10 +0100

    Loop Update

--- loops/index.js
+++ loops/index.js
@@ -1,37 +1,41 @@
-//while loop
-let i = 0;
-while (i < 3) { // shows 0, then 1, then 2
-    console.log(i);
-    i++;
-}
+// //while loop
+// let i = 0;
+// while (i < 3) { // shows 0, then 1, then 2
+//     console.log(i);
+//     i++;
+// }
 
 
-//do while
-let j = 0;
-do {
-    console.log(j);
-    j++;
-} while (i < 3)
+// //do while
+// let j = 0;
+// do {
+//     console.log(j);
+//     j++;
+// } while (j < 3)
 
 
-//for loop
-for (let i = 0; i < 10; i++) {
-    console.log(i)
-}
+// //for loop
+// for (let i = 3; i <= 10; i++) {
+//     console.log("i is now ", i)
+// }
 
 
-//for..of
-const iterable = [10, 20, 30];
+ //for..of
+ const iterable = [10, 20, 30, 40, 50];
 
-for (const value of iterable) {
-    console.log(value)
-}
+ for (const value of iterable) {
+     console.log(value)
+ }
+ const firstName = "Johnson"
+ for (const x of firstName) {
+    console.log(x)
+ }
 
+ //for ... in
 
-//for ... in
+ const userDetails = { name: "John", age: 20, city: "Lagos" };
 
-const object = { name: "John", age: 20 };
-
-for (const key in object) {
-    console.log(key)
-}
+ for (const key in userDetails) {
+    // console.log(key)
+     console.log(`The key is: ${key} and the value is: ${userDetails[key]}`)
+ }
